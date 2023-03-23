6e0657e26c01484d60b11aefe754ccb6c3306ae4
IrePin <gagistos@gmail.com>
2023-03-23 22:51:25 +0200

    Cinemachine fix, Char rotation fix

--- Assets/_Zrbo/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/_Zrbo/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -5,13 +5,17 @@ namespace _Zrbo.Scripts.StateMachines.Player
     public class PlayerStateMachine : StateMachine
     {
        [field: SerializeField] public InputReader InputReader { get; private set; }
-       
        [field: SerializeField] public Camera Cam { get; private set; }
        [field: SerializeField] public CharacterController Controller { get; private set; }
+       
+       [field: SerializeField] public Animator Animator { get; private set; }
        [field: SerializeField] public float WalkMovementSpeed { get; private set; }
+       
+       [field: SerializeField] public Transform MainCamTransform { get; private set; }
         
         private void Start()
         {
+            MainCamTransform = Cam.transform;
             SwitchState(new PlayerTestState(this));
         }
 
--- Assets/_Zrbo/Scripts/StateMachines/Player/PlayerTestState.cs
+++ Assets/_Zrbo/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -4,7 +4,6 @@ namespace _Zrbo.Scripts.StateMachines.Player
 {
     public class PlayerTestState : PlayerBaseState
     {
-        private float timer;
         public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine)
         {
         }
@@ -12,26 +11,25 @@ namespace _Zrbo.Scripts.StateMachines.Player
         public override void Enter()
         {
             stateMachine.InputReader.JumpEvent += OnJump;
-  
+            
         }
 
         public override void Tick(float deltaTime)
         {
             Vector3 moveVec = new Vector3();
-            Vector3 currentVector = stateMachine.transform.TransformDirection(moveVec).normalized;
             moveVec.x = stateMachine.InputReader.MovementValue.x;
-            moveVec.y = 0f;
             moveVec.z = stateMachine.InputReader.MovementValue.y;
+            Vector3 movement = stateMachine.transform.TransformDirection(moveVec).normalized;
             
             
-            stateMachine.Controller.Move(currentVector * stateMachine.WalkMovementSpeed * deltaTime);
-
+            stateMachine.Controller.Move(movement * (stateMachine.WalkMovementSpeed * deltaTime));
+            HandleCharacterRotation();
             // if (stateMachine.InputReader.MovementValue == Vector2.zero)
             // {
             //     return;
             // }
 
-            HandleCharacterRotation();
+
 
         }
 
@@ -44,8 +42,8 @@ namespace _Zrbo.Scripts.StateMachines.Player
         {
             float yawCamera = stateMachine.Cam.transform.rotation.eulerAngles.y;
             stateMachine.transform.rotation = Quaternion.Euler(0, yawCamera, 0);
+            
         }
-
         private void OnJump()
         {
             stateMachine.SwitchState(new PlayerTestState(stateMachine));
